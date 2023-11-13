f57fc9127c5d05a51e3709c5df3b5c7ef9c113a8
Joe Stallibrass <joe.stallibrass@effem.com>
2023-11-13 19:46:35 +0000

    added database connection

--- baristaapp/barista/models.py
+++ baristaapp/barista/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+
+class Source(models.Model):
+    id = models.AutoField(primary_key=True, db_index=True)
+    source_name = models.CharField(max_length=100, unique=True)
+    metadata = models.CharField(max_length=100, unique=True)
+
+class Beans(models.Model):
+    id = models.AutoField(primary_key=True, db_index=True)
+    source_id = models.ForeignKey(Source, on_delete=models.CASCADE)
+    bean_name = models.CharField(max_length=100, unique=True)
+    ideal_prep = models.JSONField()
